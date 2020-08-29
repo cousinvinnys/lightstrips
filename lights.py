b1a7cabd4ccdfceb47f790d7e845427f03c3f65e
Christian Peters <christianpeters31@outlook.com>
2020-08-29 14:37:30 -0400

    worky again

--- lights.py
+++ lights.py
@@ -8,6 +8,7 @@ import colorsys
 from job import Job
 from effects import *
 
+DEBUG = True
 PRINT_FRAMERATE = False
 STRIP_LENGTH = 300
 pixels = NeoPixel(board.D18, STRIP_LENGTH, auto_write=False)
@@ -22,7 +23,7 @@ def write_line(data):
 
 if __name__ == '__main__':
     jobs = []
-    jobs.append(Job(rainbow_breathe(0.1), ttl=5, name='Rainbow Breathe'))
+    jobs.append(Job(rainbow_breathe(0.005), ttl=5, name='Rainbow Breathe'))
     current_job = None
     while True:
         if PRINT_FRAMERATE:
@@ -34,24 +35,27 @@ if __name__ == '__main__':
             current_job = jobs[0]
         else:
             # Default job is off
-            print('No jobs, shutting off')
+            if DEBUG:
+                print('No jobs, shutting off')
             write_line(STRIP_LENGTH * [(0, 0, 0)])
         
         # Check if current job is running, if not, start it
         if type(current_job) == Job:
             if not current_job.is_running() and not current_job.is_dead():
-                print(f'Started job: {current_job.name} ({current_job.time_remaining()}s remaining)')
+                if DEBUG:
+                    print(f'Started job: {current_job.name} ({current_job.time_remaining()}s remaining)')
                 current_job.start()
         
-        # Get/render next line of current job
-        next_line = current_job.get_next_line()
-        if next_line is not None:
-            write_line(next_line)
-        # Kill/remove the job if past ttl
-        else:
-            print(f'Removed job: {current_job.name}')
-            jobs.pop(0)
-            current_job = None
+            # Get/render next line of current job
+            next_line = current_job.get_next_line()
+            if next_line is not None:
+                write_line(next_line)
+            # Kill/remove the job if past ttl
+            else:
+                if DEBUG:
+                    print(f'Removed job: {current_job.name}')
+                jobs.pop(0)
+                current_job = None
         
         if PRINT_FRAMERATE:
             print(f'{1 / (perf_counter() - frame_start)} fps')
