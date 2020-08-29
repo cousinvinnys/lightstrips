9213651f95e157d0b485458b326fc2794b99d9ad
Christian Peters <christianpeters31@outlook.com>
2020-08-29 14:35:33 -0400

    Worky pls

--- effects.py
+++ effects.py
@@ -3,9 +3,9 @@ import colorsys
 def hsv2rgb(h, s, v):
     return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h,s,v))
 
-def rainbow_breathe():
+def rainbow_breathe(speed=0.01):
     hue = 0
     while True:
         yield [hsv2rgb(hue, 1, 1)] * 300
-        hue += 0.01
+        hue += speed
         hue %= 1
